adb286dd08efe785b43ab81e738a5a7787135048
Nirzhuk <john.voiden@gmail.com>
2021-08-29 07:01:08 +0200

    Smart solution for a nextjs bug

--- next.config.js
+++ next.config.js
@@ -5,4 +5,8 @@ module.exports = {
   reactStrictMode: true,
   // Use the prefix in production and not development.
   assetPrefix: isProd ? '/openforge-library/' : '',
+  images: { //https://github.com/vercel/next.js/issues/21079
+    loader: "imgix",
+    path: "https://noop/",
+  }
 }
