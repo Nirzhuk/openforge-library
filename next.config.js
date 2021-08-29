47d284759871818a0d0703fec3779f9d4b8ee9c1
Nirzhuk <john.voiden@gmail.com>
2021-08-29 07:32:23 +0200

    add jekyll and basePath

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
   reactStrictMode: true,
   // Use the prefix in production and not development.
   assetPrefix: isDev ? '' : '/openforge-library/',
+  basePath: isDev ? '' : '/openforge-library/',
   images: { //https://github.com/vercel/next.js/issues/21079
     loader: "imgix",
     path: "https://noop/",
